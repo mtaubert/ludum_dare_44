acab8a3424aa29764fef426fd872a622cfa63f56
Bram <bramreth@gmail.com>
2019-04-27 05:00:45 +0100

    added imporved character sprite and looking in direction of movement

--- player_character.gd
+++ player_character.gd
@@ -5,6 +5,7 @@ signal movement_done()
 var dir = Vector2()
 export var speed = 1000
 export var limits = [0, 0, 1280, 1024]
+onready var sprite = get_node("KinematicBody2D/Sprite")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -26,6 +27,22 @@ func _process(delta):
 func move_player(location:Vector2, direction:Vector2):
 	$Movement_Tween.interpolate_property(self, "position", self.position, location, 0.3, 4, 2)
 	$Movement_Tween.start()
+	
+	#look in the direction moved
+	if direction.x:
+		if direction.x > 0:
+			sprite.frame = 2
+			sprite.flip_h = true
+		else:
+			
+			sprite.frame = 2
+			sprite.flip_h = false
+			
+	if direction.y:
+		if direction.y > 0:
+			sprite.frame = 1
+		else:
+			sprite.frame = 0
 
 #Called when tween finishes
 func movement_done():
