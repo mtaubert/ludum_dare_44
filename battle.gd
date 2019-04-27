6575598adbdb35a3af1c54145d57e49b361dca88
Bram <bramreth@gmail.com>
2019-04-27 16:07:30 +0100

    added highlights for sacrifice selection and random enemies

--- battle.gd
+++ battle.gd
@@ -3,10 +3,16 @@ extends Control
 var show_the_man = false
 onready var tween = get_node("CanvasLayer/the_man_stats/man_tween")
 export var blood_val = 20
+
+onready var enemy_0 = load("res://demon_model/commandeer_sheet.tres")
+onready var enemy_1 = load("res://demon_model/keeper_demon_sheet.tres")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$CanvasLayer/the_man_stats.set_blood(blood_val)
-
+	randomize()
+	$Node2D/enemy.frames = get("enemy_" + str(randi() % 2))
+	print("enemy_" + str(randi() % 2))
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
@@ -15,12 +21,15 @@ func _ready():
 func _on_sacrifice_pressed():
 	show_the_man = not show_the_man
 	if show_the_man:
+		$CanvasLayer/the_man_stats.toggle_buttons(true)
 		var target = $CanvasLayer/the_man_stats.rect_position
 		target.x = 80
 		tween.interpolate_property($CanvasLayer/the_man_stats, "rect_position", $CanvasLayer/the_man_stats.rect_position,  target, 0.5, Tween.TRANS_SINE, Tween.EASE_OUT)
 	else:
+		
+		$CanvasLayer/the_man_stats.toggle_buttons(false)
 		var target = $CanvasLayer/the_man_stats.rect_position
-		target.x = -80
+		target.x = -200
 		tween.interpolate_property($CanvasLayer/the_man_stats, "rect_position", $CanvasLayer/the_man_stats.rect_position,  target, 0.5, Tween.TRANS_SINE, Tween.EASE_OUT)
 	tween.start()
 
--- the_man_stats.gd
+++ the_man_stats.gd
@@ -1,8 +1,27 @@
 extends Control
-
+onready var blood = load("res://character_model/man_blood.png")
+onready var mind = load("res://character_model/man_mind.png")
+onready var heart = load("res://character_model/man_heart.png")
+onready var finger = load("res://character_model/man_fingers.png")
+onready var toe = load("res://character_model/man_toes.png")
+onready var soul = load("res://character_model/man_soul.png")
+onready var default = load("res://character_model/the_man_Stats.png")
 onready var tween = get_node("Tween")
-
 func set_blood(val):
 	#set the blood pool to a value between 0 and 100
 	tween.interpolate_property($ProgressBar, "value", $ProgressBar.value, val, 0.1, Tween.TRANS_BACK, Tween.EASE_OUT)
 	tween.start()
+	
+func toggle_buttons(visible_in):
+	$MarginContainer.visible = visible_in
+	if not visible_in:
+		$Sprite.texture = default
+	
+func _ready():
+	for item in get_tree().get_nodes_in_group("sacrifice_button"):
+		item.connect("focus_entered", self, "update_man", [item])
+		item.connect("mouse_entered", self, "update_man", [item])
+		
+func update_man(item):
+	print(item.name)
+	$Sprite.texture = get(item.name)
