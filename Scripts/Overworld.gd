6d0a1cfdff1a2553c03c37b9bf6c4a88aeac5406
Max <m.taubert2015@gmail.com>
2019-04-27 04:21:59 +0100

    Character movement on the grid

---
+++ Scripts/Overworld.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+onready var player = get_node("Mansion/Sorter/player_character")
+export(Vector2) var playerSpawn
+var playerPos
+
+func _ready():
+	player.position = $Mansion.map_to_world(playerSpawn)
+	playerPos = playerSpawn
+
+func _input(event):
+	if Input.is_action_just_pressed("ui_right"):
+		move_player(Vector2(1,0))
+	elif Input.is_action_just_pressed("ui_left"):
+		move_player(Vector2(-1,0))
+	elif Input.is_action_just_pressed("ui_up"):
+		move_player(Vector2(0,-1))
+	elif Input.is_action_just_pressed("ui_down"):
+		move_player(Vector2(0,1))
+
+func _process(delta):
+	pass
+
+func move_player(direction:Vector2):
+	playerPos += direction
+	player.position = $Mansion.map_to_world(playerPos)
--- player_character.gd
+++ player_character.gd
@@ -2,11 +2,16 @@ extends Node2D
 
 var dir = Vector2()
 export var speed = 1000
+export var limits = [0, 0, 1280, 1024]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$KinematicBody2D/Camera2D.limit_left = limits[0]
+	$KinematicBody2D/Camera2D.limit_top = limits[1]
+	$KinematicBody2D/Camera2D.limit_right = limits[2]
+	$KinematicBody2D/Camera2D.limit_bottom = limits[3]
+	set_process(false)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	dir = Vector2(0,0)
