ed2437e45a02187575ecf67e927b299df9d2b905
Bram <bramreth@gmail.com>
2019-04-27 19:25:12 +0100

    random battle backdrop

--- battle.gd
+++ battle.gd
@@ -7,12 +7,15 @@ export var blood_count = 100
 onready var enemy_0 = load("res://demon_model/commandeer_sheet.tres")
 onready var enemy_1 = load("res://demon_model/keeper_demon_sheet.tres")
 
+onready var backdrop_0 = load("res://Assets/battle_backdrop.png")
+onready var backdrop_1 = load("res://Assets/battle_backdrop_2.png")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$CanvasLayer/the_man_stats.set_blood(blood_count)
 	randomize()
 	$Node2D/enemy.frames = get("enemy_" + str(randi() % 2))
 	print("enemy_" + str(randi() % 2))
+	$TextureRect.texture = get("backdrop_" + str(randi() % 2))
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
