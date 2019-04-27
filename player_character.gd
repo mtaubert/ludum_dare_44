1b8a1b9f2b0200e2ec45a2c1a3464199a021fe3a
Bram <bramreth@gmail.com>
2019-04-27 05:38:38 +0100

    added walking animations downwards

--- player_character.gd
+++ player_character.gd
@@ -14,36 +14,44 @@ func _ready():
 	$KinematicBody2D/Camera2D.limit_right = limits[2]
 	$KinematicBody2D/Camera2D.limit_bottom = limits[3]
 	set_process(false)
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	dir = Vector2(0,0)
-	dir.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	dir.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	dir = dir.normalized()
-	$KinematicBody2D.move_and_slide(dir * speed)
 
 #Moves player to a new location
 func move_player(location:Vector2, direction:Vector2):
 	$Movement_Tween.interpolate_property(self, "position", self.position, location, 0.3, 4, 2)
 	$Movement_Tween.start()
-	
+	set_facing(direction)
 	#look in the direction moved
+	if direction.x:
+		if direction.x > 0:
+			pass
+		else:
+			pass
+			
+	if direction.y:
+		if direction.y > 0:
+			$AnimationPlayer.play("walk_down")
+		else:
+			pass
+	
+	dir = direction
+	
+#set the player to face their direction
+func set_facing(direction):
 	if direction.x:
 		if direction.x > 0:
 			sprite.frame = 2
 			sprite.flip_h = true
 		else:
-			
 			sprite.frame = 2
 			sprite.flip_h = false
-			
 	if direction.y:
 		if direction.y > 0:
 			sprite.frame = 1
 		else:
 			sprite.frame = 0
-
+	
 #Called when tween finishes
 func movement_done(object, key):
 	emit_signal("movement_done")
+	$AnimationPlayer.stop()
+	set_facing(dir)
