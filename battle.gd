d6eb8f7fcdced0e1c4ac062c9a20e37f0865cdc1
Bram <bramreth@gmail.com>
2019-04-28 12:18:35 +0100

    added combat menus for attacking and talking

--- battle.gd
+++ battle.gd
@@ -12,6 +12,11 @@ onready var wiggles = load("res://demon_model/wiggles_sheet.tres")
 onready var backdrop_0 = load("res://Assets/battle_backdrop.png")
 onready var backdrop_1 = load("res://Assets/battle_backdrop_2.png")
 # Called when the node enters the scene tree for the first time.
+
+var menu_queue = []
+var menu_loc = Vector2(524, 400)
+var menu_hide_loc = Vector2(1224, 400)
+var sub_menu_loc = Vector2(524, 640)
 func _ready():
 	randomize()
 	
@@ -38,8 +43,22 @@ func _on_sacrifice_pressed():
 		tween.interpolate_property($CanvasLayer/the_man_stats, "rect_position", $CanvasLayer/the_man_stats.rect_position,  target, 0.5, Tween.TRANS_SINE, Tween.EASE_OUT)
 	tween.start()
 
+#hide the combat meu and reveal the selected menu
+func hide_menu(type):
+	var menu = get_node(type + "_menu")
+	var target = Vector2()
+	match type:
+		"battle":
+			target = menu_hide_loc
+		_:
+			target = sub_menu_loc
+	$menu_tween.interpolate_property(menu, "rect_position", menu.rect_position,  target, 0.5, Tween.TRANS_SINE, Tween.EASE_OUT)
+	$menu_tween.start()
+
 func _on_fight_pressed():
-	hit_enemy(13)
+	hide_menu("battle")
+	menu_queue.append("fight")
+	#hit_enemy(13)
 	#blood_count += 20
 	#blood_val = blood_val %101
 	#$CanvasLayer/the_man_stats.set_blood(blood_val)
@@ -66,3 +85,35 @@ func _on_damage_tween_tween_completed(object, key):
 	if $enemy_character/enemy_health.value <= 10:
 		$enemy_character/enemy_health/AnimationPlayer.play("low_health_enemy")
 	
+
+
+func _on_talk_pressed():
+	hide_menu("battle")
+	menu_queue.append("talk")
+
+
+func _on_talk_menu_back(this):
+	print(this)
+	hide_menu("talk")
+	menu_queue.append("battle")
+
+
+
+func _on_fight_menu_back(this):
+	print(this)
+	hide_menu("fight")
+	menu_queue.append("battle")
+
+
+# brinc the named menu into the view spot
+func reveal_menu(name):
+	var menu = get_node(name + "_menu")
+	$menu_tween.interpolate_property(menu, "rect_position", menu.rect_position,  menu_loc, 0.5, Tween.TRANS_SINE, Tween.EASE_OUT)
+	$menu_tween.start()
+
+
+func _on_menu_tween_tween_completed(object, key):
+	#check the menu queue for jobs and reveal
+	if menu_queue:
+		reveal_menu(menu_queue.back())
+		menu_queue.clear()
